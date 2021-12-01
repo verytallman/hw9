3d788ca8c3d0187e7107456c11ad31c48ec1c811
verytallman <oleksiidehtiarov@gmail.com>
2021-12-01 17:03:08 +0200

    1

---
+++ src/lesson9/Task1/App.java
@@ -0,0 +1,17 @@
+package lesson9.Task1;
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+        Reader NewReader = new Reader("Дегтярев А. В.","Н125128ОНSYW","Физико-математический","12.03.1995",
+                "+38095942562");
+
+
+        NewReader.takeBook("SADASD");
+        NewReader.takeBook(2);
+        NewReader.takeBook(new String[] {"Приключения", "Словарь", "Энциклопедия"});
+
+    }
+}
